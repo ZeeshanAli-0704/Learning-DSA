a8eea687f4ff91330ca5628db525ba16de57386a
Zeeshan Ali <60847613+ZeeshanAli-0704@users.noreply.github.com>
2022-10-11 19:44:04 +0530

    Update numSubarrayProductLessThanK.js

--- TwoPointers/numSubarrayProductLessThanK.js
+++ TwoPointers/numSubarrayProductLessThanK.js
@@ -2,7 +2,7 @@
 // Given an array with positive numbers and a target number, find all of its contiguous subarrays whose product is less than the target number.
 
 // Example 1:
-
+ 
 // Input: [2, 5, 3, 10], target=30 
 // Output: [2], [5], [2, 5], [3], [5, 3], [10]
 // Explanation: There are six contiguous subarrays whose product is less than the target.
